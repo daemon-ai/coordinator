165cf8489c7b06f8b6ae38d1e19e4b064596196b
Jarrad Hope <me@jarradhope.com>
2024-08-05 14:43:59 +0200

    refactor, use fastapi, redis, rq and peewee, initial systemd setup, flake.nix development QoL

--- flake.nix
+++ flake.nix
@@ -12,17 +12,57 @@
           config = { allowUnfree = true; };
         };
 
-        devShellPackages = [
-          pkgs.gcc
-          pkgs.poetry
-          pkgs.zlib # for numpy
-          pkgs.portaudio
-          pkgs.ngrok
+        devShellPackages = with pkgs; [
+          gcc # for any python package that requires compilation
+          poetry
+          zlib # for numpy
+          ngrok
+          redis
+          jq
+          curl
+          coreutils
+          pkg-config
+          systemd
         ];
+
+        development = true;
       in
       rec {
         devShell = pkgs.mkShell {
           buildInputs = devShellPackages;
+          shellHook = if development then (with pkgs; ''
+            export LD_LIBRARY_PATH="${stdenv.cc.cc.lib}/lib"
+
+            # Prompt to delete log files with a 5-second timeout
+            echo "Clear *.log files? (Y/n)"
+            read -t 5 -n 1 -r
+            echo    # Move to a new line
+            if [[ $REPLY =~ ^[Nn]$ ]]; then
+              echo "Retaining *.log files..."
+            else
+              echo "Deleting *.log files..."
+              find . -name "*.log" -type f -delete
+            fi
+
+            redis-server > >(tee -a redis_server.log) 2>&1 &
+            REDIS_PID=$!
+
+            poetry run rq worker --with-scheduler > >(tee -a rq_worker.log) 2>&1 &
+            RQ_PID=$!
+
+            ngrok_url=$(ngrok http 8000 --log=stdout > /dev/null & sleep 2 && curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+            NGROK_PID=$!
+            
+            export NGROK_URL=$ngrok_url
+            
+            echo "REDIS_PID: $REDIS_PID"
+            echo "RQ_PID: $RQ_PID"
+            echo "NGROK_PID: $NGROK_PID"
+            echo "NGROK_URL: $NGROK_URL"
+
+            trap 'kill $NGROK_PID $RQ_PID && sleep 1 && kill $REDIS_PID && pkill -f ngrok' EXIT
+          '') else "";
         };
-      });
+      }
+    );
 }
--- src/main.py
+++ src/main.py
@@ -1,48 +1,109 @@
 
-from pathlib import Path
 import os
+import sys
+import hashlib
+import http
+import hmac
+import json
+
+from pathlib import Path
 from dotenv import load_dotenv
 
-from github import Github
-from github_bot_api import GithubApp, Webhook, Event
-from github_bot_api.flask import create_flask_app
+from github import Github, GithubIntegration
+from fastapi import FastAPI, Header, HTTPException, Request, BackgroundTasks
+from peewee import SqliteDatabase
+from redis import Redis
+from rq import Queue
+from rq_dashboard_fast import RedisQueueDashboard
+import uvicorn
+from systemd import journal
 
+
+# from .db import User
+
+# Get Module version for user agent
+current_module = sys.modules[__name__.rsplit('.', 1)[0]]
+module_version = getattr(current_module, '__version__', '0.0.0')
+
+# Load environment variables
 env_path = Path(__file__).resolve().parent.parent / '.env'
 load_dotenv(dotenv_path=env_path)
 
+ghi = GithubIntegration(
+    os.environ.get('GITHUB_APP_ID'),
+    Path(os.getenv('GITHUB_API_KEY')).read_text())
+
+wh_secret = os.environ.get('WEBHOOK_SECRET').encode('utf-8')
+
+db = SqliteDatabase(os.getenv('SQLITE_DB_PATH'), pragmas={'journal_mode': 'wal'})
+
+queue = Queue(connection=Redis())
+
+# TODO Decide which file of pull request to use for training
+# TODO Add job to queue for training
+
+# TODO Spin up RunPod machine
+# TODO manage runpod instances (start, shutdown, etc)
+# TODO monitor runpod instances
+# TODO collect results and update database
+# TODO update github with results
+
+def generate_hash_signature( secret: bytes, payload: bytes, digest_method=hashlib.sha1):
+    return hmac.new(secret, payload, digest_method).hexdigest()
+
+def connect_repo(ghi: GithubIntegration, owner: str, repo_name: str):
+    installation_id = ghi.get_installation(owner, repo_name).id
+    access_token = ghi.get_access_token(installation_id).token
+    connection = Github(login_or_token=access_token)
+    return connection.get_repo(f'{owner}/{repo_name}')
+
+def spin_up_runpod_instance():
+    # TODO spin up runpod instance
+    pass
+
+def process_payload(payload: dict):
+    print(payload)
+    if 'repository' in payload:
+        owner = payload['repository']['owner']['login']
+        repo_name = payload['repository']['name']
+        repo = connect_repo(ghi, owner, repo_name)
+        if payload.get('issue') and payload['action'] == 'opened':
+            issue = repo.get_issue(number=payload['issue']['number'])
+            issue.create_comment('Test comment')
+
 
 def main():
+    '''
+    Main entry point for the Coordinator.
+    Setup Github App and listen for webhook events.
+    '''
+    journal.send(f'Starting Coordinator {module_version}')
+
+    app = FastAPI()
+
+    dashboard = RedisQueueDashboard(os.getenv('REDIS_URL'), '/rq')
+    app.mount('/rq', dashboard)
+
+
+    @app.post('/webhook', status_code=http.HTTPStatus.ACCEPTED)
+    async def webhook(request: Request, x_hub_signature: str = Header(None), background_tasks: BackgroundTasks = BackgroundTasks()):
+        # Validate webhook signature
+        payload = await request.body()
+        signature = generate_hash_signature(wh_secret, payload)
+        if x_hub_signature != f'sha1={signature}':
+            print('Invalid signature')
+            raise HTTPException(status_code=401, detail='Authentication error.')
+        
+        print('Webhook received')
+        
+        # Parse webhook payload
+        payload = json.loads(payload)
+        background_tasks.add_task(process_payload, payload)
+
+        return {}
     
-    app = GithubApp(
-        user_agent='daemon-coordinator/0.1.0',
-        app_id=os.getenv("GITHUB_APP_ID"),
-        private_key=Path(os.getenv("GITHUB_API_KEY")).read_text(),
-    )
-
-    webhook = Webhook(secret=os.getenv('WEBHOOK_SECRET'))
-
-    def on_any_event(event: Event) -> bool:
-        print(event)
-        return True
-    webhook.listen('*', on_any_event)
+    uvicorn.run(app, host=os.getenv('UVICORN_HOST'), port=int(os.getenv('UVICORN_PORT')), log_level='info')
 
 
-    @webhook.listen('label')
-    def on_label(event: Event) -> bool:
-        print('label', event)
-        return True
-
-    @webhook.listen('pull_request')
-    def on_pull_request(event: Event) -> bool:
-        client: Github = app.installation_client(event.payload['installation']['id'])
-        repo = client.get_repo(event['repository']['full_name'])
-        pr = repo.get_pull(event['pull_request']['number'])
-        pr.create_issue_comment('Coordinator test')
-        return True
-
-    flask_app = create_flask_app(__name__, webhook, path = '/webhook')
-    flask_app.run(host=os.getenv('FLASK_RUN_HOST'), port=os.getenv('FLASK_RUN_PORT'))
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
